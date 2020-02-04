180300bbedddccf898ecc5b483791e32fe39d7c0
kstAtBitLC <klaus.stolz@bitlc.de>
2020-02-04 16:38:53 +0100

    Add project files.

---
+++ PiratenWeltEasy/GangContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiratenWeltEasy {
+    class GangContext {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        private Random zufallsGenerator = new Random ();
+
+        private List<Pirat> piraten = new List<Pirat>();
+
+        public void AddPirat (Pirat p) {
+            p.Gang = this;
+            piraten.Add (p);    
+        }
+
+        public void AddAnotherPirat () {
+            Pirat p = new Pirat () { Id=zufallsGenerator.Next (0815, 1816), Name="0815 Pirat"};
+            p.Gang = this;
+            piraten.Add (p);
+        }
+
+        public void ZeigtEichIHrPiraten() {
+            foreach ( Pirat pirat in piraten ) {
+                Console.WriteLine (pirat.Name + " " + pirat.Id + " " + pirat.Gang.Name);
+            }
+        }
+
+    }
+}
---
+++ PiratenWeltEasy/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiratenWeltEasy {
+    class Program {
+        static void Main ( string [] args ) {
+            Pirat p1 = new Pirat () { Id = 4711, Name = "Sebi Blaubart" };
+            //Console.WriteLine (p1.Name + " " + p1.Id);
+            GangContext gc1 = new GangContext () { Id=1,Name="Wolf Gang"};
+
+            gc1.AddPirat (p1);
+            gc1.AddAnotherPirat ();
+            gc1.AddAnotherPirat ();
+
+            gc1.ZeigtEichIHrPiraten ();
+
+            GangContext gc2 = new GangContext () { Id = 2, Name = "Ab Gang" };
+            for ( int i = 0 ; i < 15 ; i++ ) {
+                gc2.AddAnotherPirat ();
+            }
+
+            gc2.ZeigtEichIHrPiraten ();
+
+            Console.ReadLine ();
+        }
+    }
+}
